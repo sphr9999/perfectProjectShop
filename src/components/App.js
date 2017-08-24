45c063efdccaf2a8bb0a5213356564ea20b8ef83
unknown <sphr9999@gmail.com>
2017-08-24 13:47:14 +0700

    continue

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+export default class App extends Component {
+  render() {
+    return (
+      <View style={{ flex: 1, backgroundColor: 'gray' }}>
+      <Text>React shop Application </Text>
+      </View>
+    );
+  }
+}
